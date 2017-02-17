132842c35c9c79674c67bfb39787a4cba7fb6340
Ceasar Barbosa <ceasar.barbosa@gmail.com>
2017-02-17 15:45:01 -0600

    Initial Commit

---
+++ VenueExplorer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VenueExplorer
+//
+//  Created by Ceaz on 2/17/17.
+//  Copyright © 2017 Seize Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
